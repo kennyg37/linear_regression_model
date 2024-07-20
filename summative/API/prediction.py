25a1e5509d1632854b9e2e471f6962c97b7e88f2
kennyg37 <k.ganza@alustudent.com>
2024-07-20 23:44:45 +0200

    prediction python file to run the model

--- summative/API/prediction.py
+++ summative/API/prediction.py
@@ -1,1 +1,54 @@
 #!/usr/bin/env python3
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+import joblib
+import numpy as np
+import uvicorn
+
+model = joblib.load('multivariate_model.joblib')
+scaler = joblib.load('multivariate_scaler.joblib')
+
+app = FastAPI()
+
+class Features(BaseModel):
+    GRE_Score: int
+    TOEFL_Score: int
+    University_Rating: int
+    essay_rating: float
+    recommendation_rating: float
+    CGPA: float
+    Research: int
+
+feature_mapping = {
+    'GRE_Score': 'GRE Score',
+    'TOEFL_Score': 'TOEFL Score',
+    'University_Rating': 'University Rating',
+    'essay_rating': 'Essay rating',
+    'recommendation_rating': 'Recommendation',
+    'CGPA': 'CGPA',
+    'Research': 'Research'
+}
+
+@app.get('/')
+def read_root():
+    return {"Hello": "World"}
+
+@app.post('/predict')
+def predict(data: Features):
+    try:
+        features = np.array([[
+            getattr(data, attr) for attr in feature_mapping.keys()
+        ]])
+        
+        features_scaled = scaler.transform(features)
+        prediction = model.predict(features_scaled)
+        prediction_value = prediction[0]
+        prediction_percentage = round(prediction_value * 100, 2)
+        return {"prediction": prediction_value,
+                "predictiion percentage": prediction_percentage}
+    
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+if __name__ == '__main__':
+    uvicorn.run(app, host='127.0.0.1', port=5000)
